d95c3e36f8e9463caae78f46c6bba4dde100f8b0
shafeeque <shafeeqsha06@gmail.com>
2024-09-07 10:38:11 +0530

    practice size of datatypes

---
+++ sizeof.cpp
@@ -0,0 +1,21 @@
+
+#include <iostream>
+using namespace std;
+
+int main() {
+
+	int a;
+	float b;
+	char c;
+	double d;
+	bool e;
+
+	cout << "int " << sizeof(a);
+	cout << "fload " << sizeof(b);
+	cout << "ichar " << sizeof(c);
+	cout << "double " << sizeof(d);
+	cout << "bool " << sizeof(e);
+	cout << "int " << sizeof(a);
+
+	return 0;
+}
