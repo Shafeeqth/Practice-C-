65f04a0713576f98d1f21baf7b1028a122d830f7
shafeeque <shafeeqsha06@gmail.com>
2024-09-15 10:02:55 +0530

    c++ practice

---
+++ helo.cpp
@@ -0,0 +1,19 @@
+#include<iostream>
+using namespace std;
+
+int main() {
+	int a; 
+	// asking for value x
+	cout << "enter a value " <<endl;
+	cin >> a;
+	cout << "you entered the vlaue " << a;
+
+	// adding two values
+	int b, c;
+	cout << "now enter two vlaue to add";
+
+	cin >> b >>c;
+
+	cout << "Added amount is " << a + b;
+	return 0;
+}
